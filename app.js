a7e9043c027d8e3198c4d44ede721ed4b1233d41
JulianHernandezGit <julianhernandezok@gmail.com>
2022-02-11 23:04:27 -0300

    Modifico JS

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ app.use(express.static(pathStatic));
 
 app.listen(process.env.PORT || 5000);
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/home.html");
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.post('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
 });
